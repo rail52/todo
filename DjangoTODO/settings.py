8bb089e1784bd9036adb1c146765bbaf9ed2a35a
RAIL <railtinkoff@gmail.com>
2025-03-08 14:11:09 +0300

    + postgresql via docker

--- DjangoTODO/settings.py
+++ DjangoTODO/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,15 @@ WSGI_APPLICATION = 'DjangoTODO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'todo',
+        'PASSWORD': 'todo',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - `python3 -m venv venv` - создание виртуального окружения
 - `source venv/bin/activate` - войти в виртуальное окружение
 - `pip install -r requirements.txt` - установка зависимостей
+- `docker-compose up -d` - запустить postgresql внутри docker
