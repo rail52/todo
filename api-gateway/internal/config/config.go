4955c3fb4cdd38462b3521ec62385c714c5adf9d
RAIL <railtinkoff@gmail.com>
2025-04-14 13:57:09 +0300

    + local config

---
+++ api-gateway/internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+
+)
+
+type Config struct {
+	Env         string     `yaml:"env" env-default:"local"`
+	Timeout     time.Duration `yaml:"timeout"`
+	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("invalid CONFIG_PATH")
+	}
+	
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("%v Is Not Exist", configPath)
+	}
+	var config Config
+
+	if err := cleanenv.ReadConfig(configPath, &config); err != nil {
+		log.Fatalf("can't read config: %v", err)
+	}
+
+	return &config
+}
